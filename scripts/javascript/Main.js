83c80482b0a118a1e0fb5b8eb07f8f42aaa39fc2
Carson Crow <carsoncrow@pm.me>
2022-04-21 12:27:28 -0500

    set up directories and finished input handling

---
+++ scripts/javascript/Main.js
@@ -0,0 +1,40 @@
+
+let mousePositionX = 0;
+let mousePositionY = 0;
+let mouseDown = false;
+let spaceBarPressed = false;
+
+
+
+// while (true) {
+//     if inputHandler(["mouseDown"] == 
+// )}
+
+
+
+
+/* Keyboard event handling */
+// document.body.onkeyup = function(e){
+//     if(e.keyCode == 32){ // spacebar
+//         // TODO: pause movement
+//     }
+//     if(e.keyCode == 8){ // backspace
+//         // clear canvas
+//     }
+// }
+
+// function getMousePos(evt) {
+//     var rect = canvasElement.getBoundingClientRect();
+//     mouseX = evt.clientX - rect.left,
+//     mouseY = evt.clientY - rect.top
+// }
+
+// canvasElement.addEventListener('mousemove', function(evt) {
+//     getMousePos(evt);
+// }, false);
+// canvasElement.addEventListener('mousedown', function(evt) {
+//     mouseDown=true;
+// }, false);
+// canvasElement.addEventListener('mouseup', function(evt){
+//     mouseDown=false;
+// });
---
+++ scripts/javascript/modules/Ant.js
@@ -0,0 +1,23 @@
+
+export class Ant {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = 0;
+        this.ySpeed = 0;
+        this.size = 5;
+        this.shape = "circle";
+
+        this.lifeTime = 0;
+        this.reproduceTime = 0;
+    }
+    
+    update() {
+        
+    }
+
+    draw() {
+        
+    }
+    
+}
