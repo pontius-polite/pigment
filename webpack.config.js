6ca6c9b6a9cd04e439e348b33cf9dfff84e618ff
Carson Crow <carsoncrow@pm.me>
2023-03-03 08:43:25 -0600

    configured webpack, started refactoring

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'main.bundle.js'
+  },
+  devServer: {
+    static: path.join(__dirname),
+    compress: true,
+    port: 3000,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.json$/,
+        type: 'json'
+      },
+      {
+        test: /\.png$/,
+        type: 'asset/resource'
+      },
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+}
+
+module.exports = config
